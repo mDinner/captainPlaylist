0a695e31ae7d4e257b2bfbdd79a14fb57dc6305e
Michael Dinner <mdinner9@gmail.com>
2017-05-10 22:20:52 -0400

    add spotify user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   resources :playlists
 
   root 'playlists#index'
+
+  get '/auth/spotify/callback', to: 'users#spotify'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510014704) do
+ActiveRecord::Schema.define(version: 20170511021342) do
 
   create_table "playlists", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170510014704) do
     t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
