2053ac6ff97559da742c9062535c22e69691058f
Tyler Dinner <tylerdinner@gmail.com>
2017-05-19 00:02:21 -0400

    comments in the readme

--- README.md
+++ README.md
@@ -34,3 +34,18 @@ Things you may want to cover:
 
 
 <%= link_to 'New Playlist', new_custom_playlist_path %>
+
+
+Next steps: showing some of our playlists on the main screen
+
+first - create an object to store some static, custom playlists to display
+on the home page (ones we picked out). search our playlists by the name of the playlist if they're specific enough. otherwise either search the results or
+try getting the playlist thru the spotify user. add those playlists to the object so we can grab them custom style in the main page view file.
+
+next - we're gonna need to get a lot of cool playlists in the database. WE should write a script that gets a bunch and then adds them all so we don't have to do it manually
+
+next also - what should be on the home page when they're signed in? what about what's on their profile page?
+
+also we're gonna need a css library and a color scheme. that's u.
+
+eventually we'll use active admin to make an admin portal
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,43 +62,53 @@ class UsersController < ApplicationController
   end
 
   def spotify
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     # Now you can access user's private data, create playlists and much more
 
     # Access private data
-    spotify_user.country #=> "US"
-    spotify_user.email   #=> "example@email.com"
+    # @spotify_user.country = spotify_user.country
+    # @spotify_user.email = spotify_user.email
 
     # Create playlist in user's Spotify account
-    playlist = spotify_user.create_playlist!('my-awesome-playlist')
+    # playlist = spotify_user.create_playlist!('my-awesome-playlist')
 
     # Add tracks to a playlist in user's Spotify account
-    tracks = RSpotify::Track.search('Know')
-    playlist.add_tracks!(tracks)
-    playlist.tracks.first.name #=> "Somebody That I Used To Know"
+    # tracks = RSpotify::Track.search('Know')
+    # playlist.add_tracks!(tracks)
+    # playlist.tracks.first.name #=> "Somebody That I Used To Know"
 
     # Access and modify user's music library
-    spotify_user.save_tracks!(tracks)
-    spotify_user.saved_tracks.size #=> 20
-    spotify_user.remove_tracks!(tracks)
+    # spotify_user.save_tracks!(tracks)
+    # spotify_user.saved_tracks.size #=> 20
+    # spotify_user.remove_tracks!(tracks)
 
-    albums = RSpotify::Album.search('launeddas')
-    spotify_user.save_albums!(albums)
-    spotify_user.saved_albums.size #=> 10
-    spotify_user.remove_albums!(albums)
+    # albums = RSpotify::Album.search('launeddas')
+    # spotify_user.save_albums!(albums)
+    # spotify_user.saved_albums.size #=> 10
+    # spotify_user.remove_albums!(albums)
 
     # Use Spotify Follow features
-    spotify_user.follow(playlist)
-    #spotify_user.follows?(artists)
+    # spotify_user.follow(playlist)
+    # spotify_user.follows?(artists)
     # spotify_user.unfollow(users)
 
     # Get user's top played artists and tracks
     # spotify_user.top_artists #=> (Artist array)
     # spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
 
+    @artists = {}
+    @artists[:zeppelin] = RSpotify::Artist.search("led zeppelin").first
+
+    @songs = {}
+    @songs[:suitcase] = RSpotify::Track.search("man in a suitcase").first
+
+    # doesn't work, i wanted all playlists by a user
+    # @playlists = RSpotify::Playlist.search(@spotify_user.id)
+
+
     # Check doc for more
-    Rails.logger.info "spotify_user: #{spotify_user.inspect}"
-    @spotify_user = spotify_user
+    Rails.logger.info "spotify_user: #{@spotify_user.inspect}"
+    # @spotify_user = spotify_user
     render 'users/spotify_user'
   end
 
