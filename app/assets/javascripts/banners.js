17f747100943f00326e6390154ae7c08bf52a531
Michael Dinner <mdinner9@gmail.com>
2018-03-21 19:29:30 -0400

    style to edit user and playlists

---
+++ app/assets/javascripts/banners.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+	}
+
+	var connectionBannerCookie = getCookie('closedConnectionBanner');
+
+	$('.connect-wrap .close').on('click', function(e){
+			$('.connect-wrap').css({
+				'box-shadow': 'none',
+				'height': '0',
+				'opacity': '0',
+				'padding': '0'
+			});
+
+			document.cookie = 'closedConnectionBanner=true'; 
+	});
+
+
+	// Hide banners that have been opted out
+	if(getCookie('closedConnectionBanner') === 'true') {
+		$('.connect-wrap').css({
+			'display': 'none'
+		})
+	}
+
+	$('#myModal').on('shown.bs.modal', function () {
+	  $('#myInput').focus()
+	})
+
+})
